8bf8a1e35895aa1a360acedd8ef13e1fa1b2a1fc
Hifu <artix1995@gmail.com>
2026-08-21 20:43:08 +0200

    Briscoe: split card diagnostics from UI

---
+++ src/card.h
@@ -0,0 +1,55 @@
+#ifndef MCI_CARD_H
+#define MCI_CARD_H
+
+typedef enum CardHealth {
+    CARD_UNKNOWN = 0,
+    CARD_OK,
+    CARD_FULL,
+    CARD_UNFORMATTED,
+    CARD_FILESYSTEM_BROKEN,
+    CARD_IO_FAILURE,
+    CARD_AUTH_FAILURE,
+    CARD_DETECT_FAILURE,
+    CARD_NO_CARD
+} CardHealth;
+
+typedef enum RwStage {
+    RW_NOT_RUN = 0,
+    RW_FIND_NAME,
+    RW_VERIFY_CARD,
+    RW_OPEN_WRITE,
+    RW_WRITE,
+    RW_FLUSH,
+    RW_CLOSE_WRITE,
+    RW_OPEN_READ,
+    RW_READ,
+    RW_CLOSE_READ,
+    RW_COMPARE,
+    RW_DELETE,
+    RW_VERIFY_DELETE,
+    RW_DONE
+} RwStage;
+
+typedef struct CardReport {
+    int port;
+    int info_rc;
+    int type;
+    int free_clusters;
+    int formatted;
+    int root_rc;
+    int rw_rc;
+    int cleanup_rc;
+    int format_allowed;
+    CardHealth health;
+    RwStage rw_stage;
+} CardReport;
+
+void CardInspect(int port, CardReport *report);
+int CardFormat(int port, CardReport *report);
+
+const char *CardHealthText(CardHealth health);
+const char *CardTypeText(int type);
+const char *CardRwStageText(RwStage stage);
+const char *CardResultText(int rc);
+
+#endif /* MCI_CARD_H */
