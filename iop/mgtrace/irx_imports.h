d50e8c805a3393c3d23a8e58554f6c56722fd5f2
Hifu <artix1995@gmail.com>
2026-08-22 12:05:50 +0200

    Add dev11 tracer IRX import header

---
+++ iop/mgtrace/irx_imports.h
@@ -0,0 +1,12 @@
+#ifndef MGTRACE_IRX_IMPORTS_H
+#define MGTRACE_IRX_IMPORTS_H
+
+#include <irx.h>
+#include <loadcore.h>
+#include <sifcmd.h>
+#include <sifman.h>
+#include <sio2man.h>
+#include <sysclib.h>
+#include <thbase.h>
+
+#endif /* MGTRACE_IRX_IMPORTS_H */
