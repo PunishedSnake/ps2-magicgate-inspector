c498d35b3167eefe53ca7485aff6eb773f7f3f6d
Hifu <artix1995@gmail.com>
2026-08-24 20:57:54 +0200

    Add VMC-backed force-format interface

---
+++ src/force_format_vmc.h
@@ -0,0 +1,8 @@
+#ifndef MCI_FORCE_FORMAT_VMC_H
+#define MCI_FORCE_FORMAT_VMC_H
+
+#include "card_image.h"
+
+int MciCardForceFormatWithVmcBackup(int port, MciCardImageReport *report);
+
+#endif /* MCI_FORCE_FORMAT_VMC_H */
